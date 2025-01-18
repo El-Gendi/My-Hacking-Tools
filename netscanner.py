8d8acdee864adcf2553cb44a46ba836c1cacc221
El-Gendi <bodyelgendi3993@gmail.com>
2025-01-18 15:45:15 -0500

    improved the NetScanner program by using argparse module instead of optparse.

--- netscanner.py
+++ netscanner.py
@@ -1,37 +1,35 @@
 #!/usr/bin/env python
 
-# mostly learned/implemented from online courses/videos
-# will upgrade to <argparse> module soon
-# make sure <scapy> is installed for python 3 <
-
 import scapy.all as scapy
-import optparse
+import argparse
+import getpass
 
 def get_arguments():
-	parser = optparse.OptionParser()
-	parser.add_option("-t", "--target", dest="target", help="Target IP / IP range.")
-	(options, arguments) = parser.parse_args()
-	if not options.target:
-		ArgumentParser.error("[-] Please specify an IP target / range.")
-	return options
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", required=True, help="Target IP / IP range.")
+    return parser.parse_args()
 
 def scan(ip):
-	arp_request = scapy.ARP(pdst=ip)
-	broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-	arp_request_broadcast = broadcast/arp_request
-	answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-	clients_list = []
-	for element in answered_list:
-		client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
-		clients_list.append(client_dict)
-	return (clients_list)
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
 
 def print_result(results_list):
-	print ("IP\t\t\tMAC Address\n----------------------------------")
-	for client in results_list:
-		print(client["ip"] + "\t\t" + client["mac"])
+    print("IP\t\t\tMAC Address\n----------------------------------")
+    for client in results_list:
+        print(f"{client['ip']}\t\t{client['mac']}")
 
-options = get_arguments()
-scan_result = scan(options.target)
+args = get_arguments()
+username = getpass.getuser()
+print("----------------------------------")
+print("NetScan requested by:\t" + username)
+print("----------------------------------")
+scan_result = scan(args.target)
 print_result(scan_result)
