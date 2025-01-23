27254f484291794863e2297311f20b5deb37d98f
El-Gendi <bodyelgendi3993@gmail.com>
2025-01-23 16:08:35 -0500

    Finished the basic program. Will improve soon.

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
 
-#this tool is unfinished
-#designed to be used alongside the ARP_spoofer
+# Simple Packet Sniffer
+# Captures and displays basic packet information on a specified interface
 
 import scapy.all as scapy
 
 def sniff(interface):
-	scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+    print(f"[+] Sniffing packets on {interface}...")
+    scapy.sniff(iface=interface, store=False, prn=process_packet)
 
-def process_sniffed_packet(packet):
-	print(packet)
+def process_packet(packet):
+    if packet.haslayer(scapy.IP):
+        src_ip = packet[scapy.IP].src
+        dst_ip = packet[scapy.IP].dst
+        protocol = packet[scapy.IP].proto
+
+        if packet.haslayer(scapy.TCP):
+            print(f"[+] TCP: {src_ip} -> {dst_ip} (Port: {packet[scapy.TCP].dport})")
+        elif packet.haslayer(scapy.UDP):
+            print(f"[+] UDP: {src_ip} -> {dst_ip} (Port: {packet[scapy.UDP].dport})")
+        elif packet.haslayer(scapy.ARP):
+            print(f"[+] ARP: {packet[scapy.ARP].psrc} is at {packet[scapy.ARP].hwsrc}")
 
 sniff("eth0")
